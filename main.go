68d19f77633ca325690174292c60dd605f3dd158
Ankit Sanghi <ankitsanghi@gmail.com>
2024-07-19 09:30:09 -0700

    Add support for SMS notifications.

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 	"log"
 	"net/smtp"
 	"os"
 )
 
 func main() {
-	sendEmail()
+	useEmail := flag.Bool("email", false, "notify via email")
+	useSMS := flag.Bool("sms", false, "notify via SMS")
+	if *useEmail {
+		sendEmail()
+	}
+	if *useSMS {
+		sendSMS()
+	}
 }
 
 // sendEmail sends an email to the address in the environment variable. If either KNOCK_EMAIL_ADDRESS or
@@ -42,4 +53,31 @@ func sendEmail() {
 		return
 	}
 	log.Println("Successfully sent email " + email)
+}
+
+func sendSMS() {
+	client := twilio.NewRestClient()
+	toPhoneNumber, ok := os.LookupEnv("TO_PHONE_NUMBER")
+	if !ok {
+		log.Println("TO_PHONE_NUMBER environment variable not set")
+		return
+	}
+	twilioPhoneNumber, ok := os.LookupEnv("TWILIO_PHONE_NUMBER")
+	if !ok {
+		log.Println("TWILIO_PHONE_NUMBER environment variable not set")
+		return
+	}
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(toPhoneNumber)
+	params.SetFrom(twilioPhoneNumber)
+	params.SetBody("Hello from Golang!")
+
+	_, err := client.Api.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("SMS sent successfully!")
+	}
+
 }
